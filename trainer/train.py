99a753b6ec28509b526b41b5fd1a9aa998f9e19a
Anıl Sezgin <anil.sezgin@outlook.com>
2019-07-16 13:50:19 +0300

    Plot acc, loss and lr of the model. Used validation data picker of keras.

--- trainer/train.py
+++ trainer/train.py
@@ -13,6 +13,7 @@ from IPython.display import clear_output
 import clock
 import math
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Set Up GPU
 from tensorflow.python.client import device_lib
@@ -31,7 +32,7 @@ BATCH_SIZE = 128	                    # The number of images to process during a 
 EPOCHS = 30	                            # The number of times to iterate through the entire training set
 IMG_ROWS, IMG_COLS = IMG_SIZE, IMG_SIZE # Input Image Dimensions
 DATA_UTILIZATION = 1                    # Fraction of data which is utilized in training and testing
-TEST_RATIO = 1/4
+VALIDATION_SPLIT = 0.2
 DATA_FOLDER = "hackru"
 
 # Define function to read images from folder and convert them to gray scale
@@ -45,7 +46,7 @@ def load_images_from_folder(folder):
     return images
 
 # Define function to load data set and return testing and training data
-def loadData(test_ratio = TEST_RATIO):
+def loadData():
     data = np.zeros((0,IMG_ROWS, IMG_COLS))
     labels = []
 
@@ -54,12 +55,8 @@ def loadData(test_ratio = TEST_RATIO):
     for i, CLASS in enumerate(CLASSES):
         images = load_images_from_folder(DATA_FOLDER + '/' + CLASS)         # Load images from folder
         print(CLASS +" loaded")
-
-
-
         data = np.concatenate((data,images), axis=0)    # Add features (images) to data variable
         # print("concatenated")
-
         label = len(images) * [i]                       # Create a list of the feature labels the length of the number of images
         labels = np.concatenate((labels, label), axis=0)# Append the list of labels to the labels variable
 
@@ -72,37 +69,27 @@ def loadData(test_ratio = TEST_RATIO):
     labels = labels[0:int(len(data)*DATA_UTILIZATION)]
 
     # Split data into test and train sets
-    cutoff = int((1 - TEST_RATIO) * len(data))          # Determine the index at which to split the dataset into test and train
+    cutoff = int(len(data))          # Determine the index at which to split the dataset into test and train
     x_train = data[0:cutoff]                            # Training features
     y_train = labels[0:cutoff]                          # Training labels
-    x_test = data[cutoff:]                              # Testing features
-    y_test = labels[cutoff:]                            # Testing labels
     np.save('train',x_train)
     np.save('train_labels',y_train)
     # save numpy array as .npy formats
-    np.save('test',x_test) # saves test.npy
-    np.save('test_labels',y_test)
-    return (np.asarray(x_train), np.asarray(y_train)), (np.asarray(x_test), np.asarray(y_test))
+    return (np.asarray(x_train), np.asarray(y_train))
 
 # Load data
-(x_train, y_train), (x_test, y_test) = loadData(test_ratio = TEST_RATIO)
+(x_train, y_train) = loadData()
 
-x_test = x_test.reshape(x_test.shape[0],IMG_ROWS, IMG_COLS,1)     # Reshape x_test where 1 = number of colors
 x_train = x_train.reshape(x_train.shape[0],IMG_ROWS, IMG_COLS,1)  # Reshape x_test
 input_shape = (IMG_ROWS, IMG_COLS,1)
 x_train = x_train.astype('float32')     # Convert x_train to float32
-x_test = x_test.astype('float32')       # Convert x_test to float 32
-
 x_train /= 255                          # Scale feature values from 0-255 to values from 0-1
-x_test /= 255                           # Scale feature values from 0-255 to values from 0-1
 
 # convert class vectors to train = keras.utils.to_categorical(y_train,NUM_CLASSES = None) to binary class matrices
 # Arguments: y: Class vector to be converted into a matrix (integers from 0 to num_classes)
 #           num_classes: total number of classes
 y_train = to_categorical(y_train, num_classes = None)
-y_test = to_categorical(y_test, num_classes = None)
 print("x_train.shape = {}, y_train.shape = {}".format(x_train.shape, y_train.shape))
-print("x_test.shape = {}, y_test.shape = {}".format(x_test.shape, y_test.shape))
 inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1), name='input')
 
 x = Conv2D(32, kernel_size=(3, 3), strides=1)(inputs)
@@ -135,11 +122,19 @@ lr_decay = lambda epoch: 0.0001 + 0.02 * math.pow(1.0 / math.e, epoch / 3.0)
 decay_callback = LearningRateScheduler(lr_decay, verbose=1)
 
 history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=1,
-                    validation_data=(x_test, y_test), callbacks=[decay_callback])
+                    validation_split=VALIDATION_SPLIT, callbacks=[decay_callback])
 model.save('anil.h5')
 converter = tf.lite.TFLiteConverter.from_keras_model_file('anil.h5')
 tflite_model = converter.convert()
 open('anil.tflite', 'wb').write(tflite_model)
+
+# convert the history.history dict to a pandas DataFrame:
+hist_df = pd.DataFrame(history.history)
+
+# save to json:
+hist_json_file = 'history.json'
+with open(hist_json_file, mode='w') as f:
+    hist_df.to_json(f)
 
 # list all data in history
 print(history.history.keys())
@@ -158,4 +153,11 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for learning rate
+plt.plot(history.history['lr'])
+plt.title('model learning rate')
+plt.ylabel('learning rate')
+plt.xlabel('epoch')
+plt.legend(['lr'], loc='upper left')
 plt.show()
