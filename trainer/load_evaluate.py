6be1784323f0ebf3527b143050477c5762e1edfd
Anıl Sezgin <anil.sezgin@outlook.com>
2019-07-22 17:02:28 +0300

    Latest changes to increase accuracy and cross-validation

---
+++ trainer/load_evaluate.py
@@ -0,0 +1,29 @@
+from tensorflow.contrib import keras
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.initializers import Constant
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Dropout, Flatten, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import to_categorical
+import tensorflow as tf
+import cv2
+import numpy as np
+import random
+import os
+import os.path
+from IPython.display import clear_output
+import clock
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+x_test = np.load("test.npy")
+y_test = np.load("test_labels.npy")
+x_test = x_test.reshape(x_test.shape[0],28, 28,1)     # Reshape x_test where 1 = number of colors
+x_test = x_test.astype('float32')       # Convert x_test to float 32
+x_test /= 255                           # Scale feature values from 0-255 to values from 0-1
+y_test = to_categorical(y_test, num_classes = None)
+new_model = keras.models.load_model('withoutphdmodel.h5')
+#new_model.summary()
+score = new_model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
